eeafc44d94be90e4ff84607896da20b788049653
AzureZhen <rcraft@cyberconnects2.com>
2020-01-09 22:35:13 -0500

    code clean up

--- src/main/java/mod/azure/goldenberry/GoldenBerryMod.java
+++ src/main/java/mod/azure/goldenberry/GoldenBerryMod.java
@@ -1,8 +1,5 @@
 package mod.azure.goldenberry;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -13,11 +10,7 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod("goldenberry")
 public class GoldenBerryMod {
-	public static final Logger LOGGER = LogManager.getLogger();
 	public static final String MODID = "goldenberry";
-
-	public GoldenBerryMod() {
-	}
 
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class RegistryEvents {
@@ -25,7 +18,7 @@ public class GoldenBerryMod {
 		public static void registerItems(final RegistryEvent.Register<Item> event) {
 			event.getRegistry()
 					.registerAll(new Item(new Item.Properties().group(ItemGroup.FOOD)
-							.food(new Food.Builder().saturation(1.2F).hunger(6).build()))
+							.food(new Food.Builder().setAlwaysEdible().saturation(1.2F).hunger(6).build()))
 									.setRegistryName(new ResourceLocation(GoldenBerryMod.MODID, ("golden_berries"))));
 		}
 	}
